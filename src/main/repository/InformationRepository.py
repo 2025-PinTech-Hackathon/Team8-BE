72bee6e8f95dde1a4b34ab6ddf63842b6250d8f4
Coldot <41678750+Coldot@users.noreply.github.com>
2025-05-17 02:52:48 +0900

    fix: Information 모델의 필드 이름을 수정하여 정보 조회 메서드 개선

--- src/main/repository/InformationRepository.py
+++ src/main/repository/InformationRepository.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from src.main.domain.models import Information
+from src.main.domain.model.Information import Information
 from typing import Optional
 
 class InformationRepository:
@@ -7,4 +7,4 @@ class InformationRepository:
         self.db = db
     
     def find_by_id(self, information_id: int) -> Optional[Information]:
-        return self.db.query(Information).filter(Information.information_id == information_id).first() 
+        return self.db.query(Information).filter(Information.informationId == information_id).first() 
