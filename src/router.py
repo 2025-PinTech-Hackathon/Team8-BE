590b3c1a503e05519c18b8340bb5a2794297c6ee
Coldot <41678750+Coldot@users.noreply.github.com>
2025-05-17 03:27:22 +0900

    feat: ProfileAPIRouter를 라우터에 포함하여 프로필 관련 기능 추가

--- src/router.py
+++ src/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from src.main.router.MainAPIRouter import MainAPIRouter
 from src.main.router.FeedAPIRouter import FeedAPIRouter
 from src.main.router.ChallengeAPIRouter import ChallengeAPIRouter
+from src.main.router.ProfileAPIRouter import ProfileAPIRouter
 
 from src.main.health.router import HealthAPIRouter
 
@@ -14,3 +15,4 @@ router.include_router(HealthAPIRouter.router)
 router.include_router(MainAPIRouter)
 router.include_router(FeedAPIRouter)
 router.include_router(ChallengeAPIRouter)
+router.include_router(ProfileAPIRouter)
