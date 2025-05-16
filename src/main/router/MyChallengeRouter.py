148a02eb5ae4c05fc26f3048bd79ba8c6c69cafd
seyeon22 <sywolleych@gmail.com>
2025-05-17 05:07:29 +0900

    Feat: 친구 초대하기 모달 페이지 구현

--- src/main/router/MyChallengeRouter.py
+++ src/main/router/MyChallengeRouter.py
@@ -6,7 +6,7 @@ from src.main.domain.database import get_db
 from src.main.auth.middlewares import get_current_user
 from src.main.service._MyChallengeService import MyChallengeService
 from src.main.domain.dto.MyChallengeDto import MyChallengeReqDto
-from src.main.domain.dto.MyChallengeDto import MyChallengeRoomResDto
+from src.main.domain.dto.MyChallengeDto import MyChallengeRoomResDto, InviteCodeResponseDto
 
 router = APIRouter(
 )
@@ -20,3 +20,8 @@ async def getChallenges(challengeId: int, userId: str = Depends(get_current_user
 async def getChallengeDetail(roomId: int, userId: str = Depends(get_current_user),
                             session: AsyncSession = Depends(get_db)):
     return MyChallengeService.getChallengeDetail(session, userId, roomId)
+
+@router.get("/myChallenges/{roomId}/invite", response_model=InviteCodeResponseDto)
+async def get_invite_code(roomId: int, userId: str = Depends(get_current_user),
+                          session: AsyncSession = Depends(get_db)):
+    return MyChallengeService.get_invite_code(session, roomId)
