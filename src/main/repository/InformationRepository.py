6143e11f692536e001e26c8c909eadc9058ab382
Coldot <41678750+Coldot@users.noreply.github.com>
2025-05-17 02:50:39 +0900

    feat: InformationRepository 클래스 추가 및 정보 조회 메서드 구현

---
+++ src/main/repository/InformationRepository.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Session
+from src.main.domain.models import Information
+from typing import Optional
+
+class InformationRepository:
+    def __init__(self, db: Session):
+        self.db = db
+    
+    def find_by_id(self, information_id: int) -> Optional[Information]:
+        return self.db.query(Information).filter(Information.information_id == information_id).first() 
